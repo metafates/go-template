9dcc877fceb0afd4728b8fb9f51671b09ea51573
metafates <fates@duck.com>
2022-09-28 12:12:36 +0300

    feat: improved where cmd

--- cmd/where.go
+++ cmd/where.go
@@ -4,18 +4,29 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	"github.com/metafates/go-template/color"
 	"github.com/metafates/go-template/constant"
+	"github.com/metafates/go-template/util"
 	"github.com/metafates/go-template/where"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"os"
 )
 
+var wherePaths = []lo.Tuple2[string, func() string]{
+	{"config", where.Config},
+	{"logs", where.Logs},
+}
+
 func init() {
 	rootCmd.AddCommand(whereCmd)
 
-	whereCmd.Flags().BoolP("config", "c", false, "configuration path")
-	whereCmd.Flags().BoolP("logs", "l", false, "logs path")
-	whereCmd.MarkFlagsMutuallyExclusive("config", "logs")
+	for _, n := range wherePaths {
+		whereCmd.Flags().BoolP(n.A, string(n.A[0]), false, n.A+" path")
+	}
+
+	whereCmd.MarkFlagsMutuallyExclusive(lo.Map(wherePaths, func(t lo.Tuple2[string, func() string], _ int) string {
+		return t.A
+	})...)
+
 	whereCmd.SetOut(os.Stdout)
 }
 
@@ -23,39 +34,23 @@ var whereCmd = &cobra.Command{
 	Use:   "where",
 	Short: "Show the paths for a files related to the " + constant.App,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		headerStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color(color.HiPurple)).Render
 		yellowStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(color.Yellow)).Render
 
-		whereConfig := lo.Must(cmd.Flags().GetBool("config"))
-		whereLogs := lo.Must(cmd.Flags().GetBool("logs"))
-
-		title := func(do bool, what, arg string) {
-			if do {
-				cmd.Printf("%s %s\n", headerStyle(what+"?"), yellowStyle(arg))
+		for _, n := range wherePaths {
+			if lo.Must(cmd.Flags().GetBool(n.A)) {
+				cmd.Println(n.B())
+				return
 			}
 		}
 
-		printConfigPath := func(header bool) {
-			title(header, "Configuration", "--config")
-			cmd.Println(where.Config())
-		}
+		for i, n := range wherePaths {
+			cmd.Printf("%s %s\n", headerStyle(util.Capitalize(n.A)+"?"), yellowStyle("--"+n.A))
+			cmd.Println(n.B())
 
-		printLogsPath := func(header bool) {
-			title(header, "Logs", "--logs")
-			cmd.Println(where.Logs())
-		}
-
-		switch {
-		case whereConfig:
-			printConfigPath(false)
-		case whereLogs:
-			printLogsPath(false)
-		default:
-			printConfigPath(true)
-			cmd.Println()
-			printLogsPath(true)
-			cmd.Println()
+			if i < len(wherePaths)-1 {
+				cmd.Println()
+			}
 		}
 	},
 }
