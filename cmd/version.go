7572fbc477967fee67c9002dd515853844f45fa2
metafates <fates@duck.com>
2022-10-05 10:51:01 +0300

    feat: better looking version command

--- cmd/version.go
+++ cmd/version.go
@@ -1,20 +1,50 @@
 package cmd
 
 import (
-	"fmt"
+	"html/template"
+	"os"
+	"runtime"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/metafates/go-template/color"
 	"github.com/metafates/go-template/constant"
 	"github.com/spf13/cobra"
-	"runtime"
 )
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	versionCmd.SetOut(os.Stdout)
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of the " + constant.App,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s version %s %s/%s\n", constant.App, constant.Version, runtime.GOOS, runtime.GOARCH)
+		versionInfo := struct {
+			Version  string
+			OS       string
+			Arch     string
+			App      string
+			Compiler string
+		}{
+			Version:  constant.Version,
+			App:      constant.App,
+			OS:       runtime.GOOS,
+			Arch:     runtime.GOARCH,
+			Compiler: runtime.Compiler,
+		}
+
+		t, err := template.New("version").Funcs(map[string]any{
+			"faint":   lipgloss.NewStyle().Faint(true).Render,
+			"bold":    lipgloss.NewStyle().Bold(true).Render,
+			"magenta": lipgloss.NewStyle().Foreground(color.Purple).Render,
+		}).Parse(`{{ magenta "▇▇▇" }} {{ magenta .App }} 
+
+  {{ faint "Version" }}   {{ bold .Version }}
+  {{ faint "Platform" }}  {{ bold .OS }}/{{ bold .Arch }}
+  {{ faint "Compiler" }}  {{ bold .Compiler }}
+`)
+		handleErr(err)
+		handleErr(t.Execute(cmd.OutOrStdout(), versionInfo))
 	},
 }
