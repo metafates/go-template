0662636511351763805f3f834913dc4cfe14a153
metafates <fates@duck.com>
2022-10-15 22:53:46 +0300

    refactor: make where cmd more generic

--- cmd/where.go
+++ cmd/where.go
@@ -4,34 +4,37 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	"github.com/metafates/go-template/color"
 	"github.com/metafates/go-template/constant"
-	"github.com/metafates/go-template/util"
 	"github.com/metafates/go-template/where"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"os"
-	"strings"
 )
 
-var wherePaths = []lo.Tuple2[string, func() string]{
-	{"config", where.Config},
-	{"logs", where.Logs},
+type whereTarget struct {
+	name  string
+	where func() string
+
+	argShort, argLong string
 }
 
-func init() {
-	for _, p := range wherePaths {
-		p.A = strings.ToLower(p.A)
-	}
+var wherePaths = []whereTarget{
+	{"Config", where.Config, "c", "config"},
+	{"Logs", where.Logs, "l", "logs"},
 }
 
 func init() {
 	rootCmd.AddCommand(whereCmd)
 
 	for _, n := range wherePaths {
-		whereCmd.Flags().BoolP(n.A, string(n.A[0]), false, n.A+" path")
+		if n.argShort != "" {
+			whereCmd.Flags().BoolP(n.argLong, n.argShort, false, n.name+" path")
+		} else {
+			whereCmd.Flags().Bool(n.argLong, false, n.name+" path")
+		}
 	}
 
-	whereCmd.MarkFlagsMutuallyExclusive(lo.Map(wherePaths, func(t lo.Tuple2[string, func() string], _ int) string {
-		return t.A
+	whereCmd.MarkFlagsMutuallyExclusive(lo.Map(wherePaths, func(t whereTarget, _ int) string {
+		return t.argLong
 	})...)
 
 	whereCmd.SetOut(os.Stdout)
@@ -45,15 +48,15 @@ var whereCmd = &cobra.Command{
 		yellowStyle := lipgloss.NewStyle().Foreground(color.Yellow).Render
 
 		for _, n := range wherePaths {
-			if lo.Must(cmd.Flags().GetBool(n.A)) {
-				cmd.Println(n.B())
+			if lo.Must(cmd.Flags().GetBool(n.argLong)) {
+				cmd.Println(n.where())
 				return
 			}
 		}
 
 		for i, n := range wherePaths {
-			cmd.Printf("%s %s\n", headerStyle(util.Capitalize(n.A)+"?"), yellowStyle("--"+n.A))
-			cmd.Println(n.B())
+			cmd.Printf("%s %s\n", headerStyle(n.name+"?"), yellowStyle("--"+n.argLong))
+			cmd.Println(n.where())
 
 			if i < len(wherePaths)-1 {
 				cmd.Println()
